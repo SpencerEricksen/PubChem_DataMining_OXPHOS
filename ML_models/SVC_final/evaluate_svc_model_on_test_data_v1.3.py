04e46d5a4b4b9c553855a8cbfc23a04d6a21eda6
Spencer S. Ericksen <spencer.ericksen@gmail.com>
2024-05-08 14:40:23 -0500

    Add files via upload
    build final SVC models, evaluate on test data

---
+++ ML_models/SVC_final/evaluate_svc_model_on_test_data_v1.3.py
@@ -0,0 +1,103 @@
+
+import sys
+import pickle
+import pandas as pd
+import numpy as np
+from sklearn.metrics import f1_score, matthews_corrcoef, average_precision_score, roc_auc_score, balanced_accuracy_score
+import sys
+
+def enrichment_factor( labels_arr, scores_arr, percentile ):
+    '''calculate the enrichment factor based on some upper fraction
+       of library ordered by docking scores. upper fraction is determined
+       by percentile (actually a fraction of value 0.0-1.0). Also calculate
+       the theoretical maximum EF given the percentile threshold'''
+    sample_size = int(labels_arr.shape[0] * percentile)  # determine number mols in subset
+    indices = np.argsort(scores_arr)[::-1][:sample_size] # get the index positions for these in library
+    n_actives = np.nansum(labels_arr)                    # count number of positive labels in library
+    n_actives_sample = np.nansum( labels_arr[indices] )    # count number of positive labels in subset
+    if n_actives > 0.0:
+       ef = float(n_actives_sample) / n_actives / percentile # calc EF at percentile
+       ef_max = min(n_actives, sample_size) / (n_actives * percentile)
+    else:
+       ef = np.nan
+       ef_max = np.nan
+    return n_actives, ef, ef_max
+
+def normalized_enrichment_factor_single(labels_arr, scores_arr, percentile):
+    n_actives, ef, ef_max = enrichment_factor(labels_arr, scores_arr, percentile)
+    return ef, ef/ef_max
+
+try:
+    feat_type = sys.argv[1]  # 'desc', 'fps', or 'both'
+    model_name = sys.argv[2] # "model_l2_lbfgs_C0.00599.pkl"
+    test_data = sys.argv[3]  # "../data/old_85_15_split/test_data.pkl"
+except:
+    print('provide feat_type (desc, fps, both), model_file (pkl), and test_data (pkl) as arguments')
+    sys.exit(1)
+
+# load model
+mymodel = pickle.load( open(model_name, 'rb') )
+
+# load held-out test data for model evaluation
+df = pd.read_pickle( test_data )
+molid_list = df.PUBCHEM_CID.tolist()
+
+# features
+col_list = df.columns.tolist()
+if feat_type == 'desc':
+    feat_cols = [ c for c in col_list[2:] if 'desc' in str(c) ]
+    feat_cols.remove('desc_Ipc')
+    df_X = df[feat_cols]
+    # replace NaNs with column means (impute)
+    df_X = df_X.fillna( df_X.mean() )
+    # load the scaler previously fit on the training data
+    scaler = pickle.load( open('../data/stand_scaler_desc_training_data.pkl', 'rb') )
+    X = scaler.transform( df_X )
+elif feat_type == 'fps':
+    feat_cols = [ c for c in col_list[2:] if 'desc' not in str(c) ]
+    X = df[feat_cols].values
+    scaler = pickle.load( open('../data/stand_scaler_fps_training_data.pkl', 'rb') )
+    X = scaler.transform(X)
+elif feat_type == 'both':
+    # get dataframe of desc features
+    desc_cols = [ c for c in col_list[2:] if 'desc' in str(c) ]
+    desc_cols.remove('desc_Ipc')
+    df_X1 = df[desc_cols]
+    # replace NaNs with column means (impute)
+    df_X1 = df_X1.fillna( df_X1.mean() )
+    scaler = pickle.load( open('../data/stand_scaler_desc_training_data.pkl', 'rb') )
+    X1 = scaler.transform( df_X1 )
+    df_X1 = pd.DataFrame( X1, columns=desc_cols, index=molid_list )
+    # get data frame of fps features
+    fps_cols = [ c for c in col_list[2:] if 'desc' not in str(c) ]
+    X2 = df[fps_cols].values
+    scaler = pickle.load( open('../data/stand_scaler_fps_training_data.pkl', 'rb') )
+    X2 = scaler.transform(X2)
+    df_X2 = pd.DataFrame( X2, columns=fps_cols, index=molid_list )
+    # merge desc and fps into single dataframe, get feature array
+    df_X12 = pd.concat( [df_X1, df_X2], axis=1 )
+    X = df_X12.values
+
+# labels
+y = df.label.values
+#weird issue with int type in y, had to set type to match X
+y = np.array( y, np.int64 )
+
+# get predicted labels (0/1)
+y_pred = mymodel.predict(X)
+
+# get predicted probabilities (needed for rocauc and prauc)
+y_score = mymodel.predict_proba(X)[:,1]
+#y_score = loaded_model.decision_function(X)
+
+# binary classification metrics
+f1 = f1_score( y, y_pred )
+mcc = matthews_corrcoef( y, y_pred )
+bac = balanced_accuracy_score( y, y_pred )
+
+# continuous classification metrics
+avgprec = average_precision_score( y, y_score )
+rocauc = roc_auc_score( y, y_score )
+ef1, nef1 = normalized_enrichment_factor_single( y, y_score, 0.01 )
+print('model,balanced_acc,avg_prec,roc_auc,F1,MCC,EF1,NEF1')
+print(model_name, bac, avgprec, rocauc, f1, mcc, ef1, nef1)
---
+++ ML_models/SVC_final/svc_dump_model_general.py
@@ -0,0 +1,119 @@
+
+import sys
+import pandas as pd
+import numpy as np
+import pickle
+from sklearn.svm import SVC
+
+try:
+    feat_type = sys.argv[1]    # feature types -- 'fps', 'desc' or 'both'
+    kernel_type = sys.argv[2]    # 'linear','rbf','sigmoid','poly'
+    c = float(sys.argv[3])   # 'regularization factor C'
+    g = sys.argv[4]    # kernel coef for 'rbf', 'poly', 'sigmoid'
+    cf = sys.argv[5]   # term added to 'sigmoid' or 'poly' kernels
+    d = sys.argv[6]   # degree of kernel function in 'poly'
+except:
+    print("provide feature type: 'desc', 'fps', or 'both'")
+    sys.exit(1)
+
+# load data, get features
+df = pd.read_pickle('../data/training_data.pkl')
+# in descriptors, had to replace infinite values with NaNs
+df = df.replace( [np.inf, -np.inf], np.nan )
+# to get around issues of memory overflow
+#df = df.sample( frac=0.33 )
+# get molids
+molid_list = df.PUBCHEM_CID.tolist()
+
+# features
+col_list = df.columns.tolist()
+if feat_type == 'desc':
+    feat_cols = [ c for c in col_list[2:] if 'desc' in str(c) ]
+    feat_cols.remove('desc_Ipc')
+    df_X = df[feat_cols]
+    # replace NaNs with column means (impute)
+    df_X = df_X.fillna( df_X.mean() )
+    # load the scaler previously fit on the training data
+    scaler = pickle.load( open('../data/stand_scaler_desc_training_data.pkl', 'rb') )
+    X = scaler.transform( df_X )
+elif feat_type == 'fps':
+    feat_cols = [ c for c in col_list[2:] if 'desc' not in str(c) ]
+    X = df[feat_cols].values
+    scaler = pickle.load( open('../data/stand_scaler_fps_training_data.pkl', 'rb') )
+    X = scaler.transform(X)
+elif feat_type == 'both':
+    # get dataframe of desc features
+    desc_cols = [ c for c in col_list[2:] if 'desc' in str(c) ]
+    desc_cols.remove('desc_Ipc')
+    df_X1 = df[desc_cols]
+    # replace NaNs with column means (impute)
+    df_X1 = df_X1.fillna( df_X1.mean() )
+    scaler = pickle.load( open('../data/stand_scaler_desc_training_data.pkl', 'rb') )
+    X1 = scaler.transform( df_X1 )
+    df_X1 = pd.DataFrame( X1, columns=desc_cols, index=molid_list )
+    # get data frame of fps features
+    fps_cols = [ c for c in col_list[2:] if 'desc' not in str(c) ]
+    X2 = df[fps_cols].values
+    scaler = pickle.load( open('../data/stand_scaler_fps_training_data.pkl', 'rb') )
+    X2 = scaler.transform(X2)
+    df_X2 = pd.DataFrame( X2, columns=fps_cols, index=molid_list )
+    # merge desc and fps into single dataframe, get feature array
+    df_X12 = pd.concat( [df_X1, df_X2], axis=1 )
+    X = df_X12.values
+
+# labels
+y = df.label.values
+#weird issue with int type in y, had to set type to match X
+y = np.array( y, np.int64 )
+
+# SVC params
+# C = regularization param--strength of reg inversely proportional to C (default C=1.0)
+# kernel = 'rbf' (default), 'poly', 'sigmoid', 'linear' --  something like Jaccard or Tanimoto might make sense
+# degree = 3 (default) -- degree of polynomial kernel--only used if kernel = 'poly'
+# gamma = kernel coef for 'rbf', 'poly', or 'sigmoid',
+#       = 'scale' (default) -> 1/(n_feat * X.var()),
+#       = 'auto' -> 1/n_feat, or
+#       =  float (must be non-negative)
+
+key_c = '{:.3f}'.format(c)
+
+if type(g) is str:
+    key_g = g
+    if g == 'null':
+        g = None
+else:
+    g = float(g)
+    key_g = '{:.3f}'.format( g )
+
+if cf == 'null':
+    key_cf = cf
+    cf = None
+else:
+    cf = float(cf)
+    key_cf = '{:.3f}'.format( cf )
+
+if d == 'null':
+    key_d = d
+    d = None
+else:
+    d = int(d)
+    key_d = str( d )
+
+model_name = "svc_" + kernel_type + "_" + feat_type + "_C_" + key_c + "_gamma_" + key_g + "_coef0_" + key_cf + "_degree_" + key_d
+
+if kernel_type == 'linear':
+    model = SVC( C=c, kernel=kernel_type, class_weight='balanced', probability=True )
+elif kernel_type == 'rbf':
+    model = SVC( C=c, kernel=kernel_type, gamma=g, class_weight='balanced', probability=True )
+elif kernel_type == 'sigmoid':
+    model = SVC( C=c, kernel=kernel_type, gamma=g, coef0=cf, class_weight='balanced', probability=True )
+elif kernel_type == 'poly':
+    model = SVC( C=c, kernel=kernel_type, gamma=g, coef0=cf, degree=d, class_weight='balanced', probability=True )
+
+# train on full train set (80% of the total data)
+model.fit( X, y )
+
+# save the model to disk
+model_filename = "model_" + model_name + ".pkl"
+pickle.dump( model, open( model_filename, 'wb'))
+
